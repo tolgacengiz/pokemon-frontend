da02c04959f3e92bacda9940e9a8f989d11ad302
tolgacengiz <64279187+tolgacengiz@users.noreply.github.com>
2020-06-23 09:53:14 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
